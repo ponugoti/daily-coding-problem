9abbc9e2b2d3dc0157e48a98a3b31f408e77d008
Jesse Ponugoti <jesse.ponugoti@usask.ca>
2018-07-31 15:58:13 -0600

    Finish 4

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-# May 31, 2018
+ # May 31, 2018
 
 """
 This problem was asked by Google.
@@ -19,3 +19,40 @@ The following test should pass:
 node = Node('root', Node('left', Node('left.left')), Node('right'))
 assert deserialize(serialize(node)).left.left.val == 'left.left'
 """
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def serialize(root):
+    def helper(node, lvl=0):
+        if not node:
+            return ''
+        seperator = '*-' + str(lvl) + '-*'
+        left = helper(node.left, lvl+1)
+        right = helper(node.right, lvl+1)
+        return str(node.val) + seperator + left + seperator + right
+
+    return helper(root)
+
+def deserialize(s):
+    def helper(st, lvl=0):
+        node = st.split('*-' + str(lvl) + '-*')
+        try:
+            val, left, right = node[0], node[1], node[2]
+        except IndexError:
+            # the tree has been travesed exhaustively
+            return None
+        return Node(val, helper(left, lvl+1), helper(right, lvl+1))
+
+    return helper(s)
+
+if __name__ == '__main__':
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+
+    # root*-0-*left*-1-*left.left*-2-**-2-**-1-**-0-*right*-1-**-1-*
+    # print('serialized:', serialize(node))
+
+    assert deserialize(serialize(node)).left.left.val == 'left.left'
